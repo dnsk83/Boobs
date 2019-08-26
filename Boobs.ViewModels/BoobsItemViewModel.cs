853d48e457f64af63e86436be12cdce917fb3d9e
Daniil.Mikhin <damihin@gmail.com>
2019-08-26 15:24:38 +0300

    Get full size image for full size page

--- Boobs.ViewModels/BoobsItemViewModel.cs
+++ Boobs.ViewModels/BoobsItemViewModel.cs
@@ -17,11 +17,20 @@ namespace Boobs.ViewModels
         public string Model => $"Модель: {BoobsItem.Model}";
 
         public string Preview => $"http://media.oboobs.ru/{BoobsItem.Preview}";
+        public string FullSize => GetFullSize();
 
         public long Id => BoobsItem.Id;
 
         // public long Rank { get; set; }
 
         // public string Author { get; set; }
+
+        private string GetFullSize()
+        {
+            var sub = "_preview";
+            var s1 = Preview.Substring(0, Preview.IndexOf(sub));
+            var s2 = Preview.Substring(s1.Length + sub.Length);
+            return s1 + s2;
+        }
     }
 }
