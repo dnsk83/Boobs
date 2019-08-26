fd9365cb63cb18e6480d80254334df3596bf987a
Daniil.Mikhin <damihin@gmail.com>
2019-08-26 14:16:11 +0300

    Adding a sorting to main page

--- Boobs.ViewModels/BoobsListViewModel.cs
+++ Boobs.ViewModels/BoobsListViewModel.cs
@@ -10,20 +10,58 @@ namespace Boobs.ViewModels
     public class BoobsListViewModel : BaseViewModel
     {
         public ObservableCollection<BoobsItemViewModel> BoobsList { get; set; }
+        public List<OrderByItem> SortByList { get; private set; }
+        OrderByItem selectedSortingCriteria;
+        public OrderByItem SelectedSortingCriteria
+        {
+            get { return selectedSortingCriteria; }
+            set
+            {
+                if (selectedSortingCriteria != value)
+                {
+                    selectedSortingCriteria = value;
+                    OnSortingChangedAsync();
+                    NotifyPropertyChanged(nameof(SelectedSortingCriteria));
+                }
+            }
+        }
+
         public BoobsListViewModel()
         {
             BoobsList = new ObservableCollection<BoobsItemViewModel>();
             GetBoobsAsync();
+            LoadSortbyList();
+
+            NotifyPropertyChanged(nameof(SortByList));
         }
-        private async Task GetBoobsAsync()
+
+        private void LoadSortbyList()
+        {
+            SortByList = new List<OrderByItem>();
+            SortByList.Add(new OrderByItem(OrderBy.RankAsc, "Rating +"));
+            SortByList.Add(new OrderByItem(OrderBy.RankDesc, "Rating -"));
+            SortByList.Add(new OrderByItem(OrderBy.InterestAsc, "Interest +"));
+            SortByList.Add(new OrderByItem(OrderBy.InterestDesc, "Interest -"));
+            SortByList.Add(new OrderByItem(OrderBy.Random, "Random"));
+            SortByList.Add(new OrderByItem(OrderBy.Id, "Photo ID"));
+        }
+
+        private async Task GetBoobsAsync(OrderBy orderBy=OrderBy.Random)
         {
             var service = new BoobsService();
-            var list = await service.GetBoobsAsync(10, 10);
+            var list = await service.GetBoobsAsync(10, 10, orderBy);
+            BoobsList.Clear();
             foreach (var item in list)
             {
                 BoobsList.Add(new BoobsItemViewModel(item));
             }
             NotifyPropertyChanged(nameof(BoobsList));
         }
+
+        private async Task OnSortingChangedAsync()
+        {
+            await GetBoobsAsync(SelectedSortingCriteria.Key);
+        }
+
     }
 }
---
+++ Boobs.ViewModels/OrderByItem.cs
@@ -0,0 +1,16 @@
+﻿using Boobs.Core;
+
+namespace Boobs.ViewModels
+{
+    public class OrderByItem
+    {
+        public OrderByItem(OrderBy orderBy, string Name)
+        {
+            this.Key = orderBy;
+            this.Name = Name;
+        }
+
+        public OrderBy Key { get; set; }
+        public string Name { get; set; }
+    }
+}
